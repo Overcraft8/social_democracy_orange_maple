71b5225a7bb558f05197ac1b10ea355f58da0968
EJ Larracas <EJLarracas@outlook.com>
2026-02-14 16:32:37 -0600

    Logo image CCF

--- out/html/data.js
+++ out/html/data.js
@@ -1,13 +1,13 @@
 const tooltipList = [{
     searchString: "CCF",
-    explanationText: "<img src=img/ps_a.png> The Co-operative Commonwealth Federation - Left"
+    explanationText: "<img src=img/CCF_logo.png> The Co-operative Commonwealth Federation - Left"
 }];
 
 
 const colourList = [{
         word: "CCF",
         style: "color: #BA0000; font-weight: bold;",
-        img: "img/ps.png" // this line is not mandatory
+        img: "img/CCF_logo.png" // this line is not mandatory
     }
 ];
 
